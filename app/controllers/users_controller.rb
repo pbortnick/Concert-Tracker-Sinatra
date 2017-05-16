f6ffa5b3c6007cc2623e93291727e08f4ca3001f
Pamela Bortnick <pbortnick@gmail.com>
2017-05-16 03:15:57 +0000

    updated users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,35 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/signup' do
+    if params[:username] != "" || params[:email] != "" || params[:password] != ""
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/concerts'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/concerts'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/concerts'
+    else
+      redirect to '/signup'
+    end
+  end
+
+
 
 end
